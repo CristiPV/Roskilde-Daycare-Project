45185edc1738d21b2b00d962f2041d592894a3c7
Cristian <cristi.valentin29@gmail.com>
2020-03-27 11:21:59 +0100

    Final Push

--- src/App.java
+++ src/App.java
@@ -1,4 +1,14 @@
 public class App {
+    /*
+    Dependency: jdbc-connector.jar.
+    It has to be downloaded and added in a ./lib/ directory.
+    IntelliJ installation :
+    1. Go to File.
+    2. Select Project Structure.
+    3. On the left tab, select Modules.
+    4. Click on the right-side +, then select JARs or Directories
+    5. Select the connector from the newly created directory, check the box & click accept.
+    */
     private static Controller controller;
     private static final String URLDB = "personal-database.cfi7hnmvjvlo.eu-central-1.rds.amazonaws.com";
     private static final String SCHEMA = "roskilde_daycare";
